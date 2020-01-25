6519648ebee5596247c98859274c5304ae006dcf
nitz333 <59187931+nitz333@users.noreply.github.com>
2020-01-24 18:55:24 -0600

    Update environment.prod.ts
    Omití el valor de la apiKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   urlFirestoreGoty: 'https://us-central1-firestore-goty-cc76a.cloudfunctions.net',
   firebase: {
-    apiKey: "AIzaSyDzo9_UKjnMXfVrat8A7dkh22ERPEW_5MI",
+    apiKey: "[VER CUAL ES EN LA CONSOLA DE FIREBASE]",
     authDomain: "firestore-goty-cc76a.firebaseapp.com",
     databaseURL: "https://firestore-goty-cc76a.firebaseio.com",
     projectId: "firestore-goty-cc76a",
